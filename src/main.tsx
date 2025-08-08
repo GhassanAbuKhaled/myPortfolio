3ffc0ca529b65a5ac19b5464bc5288632ad7a820
Ghassan <ghassan.adnanabukhaled@gmail.com>
2025-08-08 11:31:04 +0200

    Fix Firefox mobile display issues with viewport and CSS fixes

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { LanguageProvider } from './components/LanguageProvider';
-import './index.css';
+import './index.css'
+import './styles/firefox-fixes.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
